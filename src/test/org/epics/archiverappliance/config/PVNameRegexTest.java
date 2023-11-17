64e3f02fe3c574006bc988506228ca3add647d0b
Sky Brewer <sky.brewer@ess.eu>
2023-11-17 15:28:45 +0100

    Allow usage of "'" in a pvname Allow pvNames that have nonURLSafe chars

--- src/test/org/epics/archiverappliance/config/PVNameRegexTest.java
+++ src/test/org/epics/archiverappliance/config/PVNameRegexTest.java
@@ -1,8 +1,12 @@
 package org.epics.archiverappliance.config;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Small unit test to make sure the regex that checks for valid pv names does something reasonable
@@ -11,43 +15,58 @@ import org.junit.Test;
  */
 public class PVNameRegexTest {
 
-	@Test
-	public void test() {
-		assertTrue("Checking for null pvName failed", !PVNames.isValidPVName(null));
-		assertTrue("Checking for empty pvName failed", !PVNames.isValidPVName(""));
-		String pvName = "";
-		pvName = "archappl:arch:sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch:sine1"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch:sine1.HIHI"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch:sine100"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl_arch_sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl-arch-sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl+arch+sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl[arch]sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl<arch>sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch;sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar/h:sine"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		// Add for FRIB
-		pvName = "P#6:000357"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		// Add for BNL
-		pvName = "LN-AM{RadMon:1}Lvl:Raw-I"; assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
-		
-		// Let's test some invalid ones
-		pvName = "\"archappl:arch:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch:sine\""; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar'h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar!h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar$h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar%h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar*h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar\\h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar|h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar'h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar?h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar&h:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:ar@ch:sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:(arch):sine"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-		pvName = "archappl:arch:sine=1.0"; assertTrue("Invalid pvName is deemed valid " + pvName, !PVNames.isValidPVName(pvName));
-	}
+    @Test
+    public void testValidPVNames() {
 
+        List<String> pvNames = List.of(
+                "archappl:arch:sine",
+                "archappl:arch:sine1",
+                "archappl:arch:sine1.HIHI",
+                "archappl:arch:sine100",
+                "archappl_arch_sine",
+                "archappl-arch-sine",
+                "archappl+arch+sine",
+                "archappl[arch]sine",
+                "archappl<arch>sine",
+                "archappl:arch;sine",
+                "archappl:ar/h:sine",
+                "P#6:000357",
+                "LN-AM{RadMon:1}Lvl:Raw-I",
+                "TBGR001E01.{'dbnd':{'abs':1}}",
+                "archappl:ar'h:sine",
+                "CrS-PHS:Cryo-D-0100:AmbientTemp.{'dbnd':{'rel':0.1}}");
+        for (String pvName : pvNames) {
+            assertTrue("Valid pvName is deemed invalid " + pvName, PVNames.isValidPVName(pvName));
+            assertEquals(pvName, PVNames.normalizePVName(pvName));
+        }
+    }
+
+    @Test
+    public void testNormalizePVName() {
+        String pvName = "CrS-PHS:Cryo-D-0100:AmbientTemp.VAL";
+        assertEquals("CrS-PHS:Cryo-D-0100:AmbientTemp", PVNames.normalizePVName(pvName));
+    }
+
+    @Test
+    public void testInvalidPVNames() {
+        List<String> pvNames = List.of(
+                "\"archappl:arch:sine",
+                "archappl:arch:sine\"",
+                "archappl:ar!h:sine",
+                "archappl:ar$h:sine",
+                "archappl:ar%h:sine",
+                "archappl:ar*h:sine",
+                "archappl:ar\\h:sine",
+                "archappl:ar|h:sine",
+                "archappl:ar?h:sine",
+                "archappl:ar&h:sine",
+                "archappl:ar@ch:sine",
+                "archappl:(arch):sine",
+                "archappl:arch:sine=1.0",
+                "");
+        for (String pvName : pvNames) {
+            assertFalse("Invalid pvName is deemed valid " + pvName, PVNames.isValidPVName(pvName));
+        }
+        assertFalse("null is deemed invalid", PVNames.isValidPVName(null));
+    }
 }

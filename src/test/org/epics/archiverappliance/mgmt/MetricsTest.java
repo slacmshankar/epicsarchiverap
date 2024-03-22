670a24d18071ed62f1eff24f300a69af71461cbf
Sky Brewer <sky.brewer@ess.eu>
2024-03-22 11:24:27 +0100

    Basic test PVDetails api

--- src/test/org/epics/archiverappliance/mgmt/MetricsTest.java
+++ src/test/org/epics/archiverappliance/mgmt/MetricsTest.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.epics.archiverappliance.SIOCSetup;
 import org.epics.archiverappliance.TomcatSetup;
+import org.epics.archiverappliance.retrieval.RetrievalMetrics;
 import org.epics.archiverappliance.utils.ui.GetUrlContent;
 import org.json.simple.JSONArray;
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -19,6 +21,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static java.util.Map.entry;
 import static org.epics.archiverappliance.engine.V4.PVAccessUtil.waitForStatusChange;
 
 @Tag("integration")
@@ -97,20 +100,80 @@ public class MetricsTest {
         partialAssertMap(expectedMetrics, actual);
     }
 
-    private static Map<String, String> convertToStringMap(List<Map<String, String>> expectedMetrics) {
-        return expectedMetrics.stream()
-                .collect((Collectors.toMap(m -> m.get("source") + m.get("name"), m -> m.get("value"))));
+    private static void assertPVDetailsMatch(Map<String, String> expectedMetrics) {
+        JSONArray metricsResponse = GetUrlContent.getURLContentAsJSONArray(
+                MetricsTest.mgmtUrl + "getPVDetails?pv=" + URLEncoder.encode(pvName, StandardCharsets.UTF_8));
+        List<Map<String, String>> metricsResult = (List<Map<String, String>>) metricsResponse;
+
+        Map<String, String> actual = convertToStringMap(metricsResult);
+        partialAssertMap(expectedMetrics, actual);
     }
+
+    private static Map<String, String> convertToStringMap(List<Map<String, String>> actualMetrics) {
+        logger.info("Actual metrics are: " + actualMetrics);
+        return actualMetrics.stream()
+                .collect((Collectors.toMap(
+                        m -> m.get("source") + m.get("name"), m -> StringUtils.nullSafeToString(m.get("value")))));
+    }
+
+    String retrievalSource = "retrieval";
+    String engineSource = "engine";
+    String mgmtSource = "mgmt";
+    String etlSource = "etl";
+    String pvSource = "pv";
 
     @Test
     void testApplianceMetricsForAppliance() {
-
-        Map<String, String> expectedMetrics = Map.of("mgmtAppliance Identity", "appliance0");
+        Map<String, String> expectedMetrics = Map.of(
+                "mgmt" + "Appliance Identity",
+                "appliance0",
+                engineSource + "Total PV Count",
+                "0",
+                engineSource + "Disconnected PV count",
+                "0",
+                engineSource + "Connected PV count",
+                "0",
+                engineSource + "Paused PV count",
+                "0",
+                engineSource + "Total channels",
+                "0",
+                retrievalSource + RetrievalMetrics.NUMBER_OF_RETRIEVAL_REQUESTS,
+                "0",
+                retrievalSource + RetrievalMetrics.NUMBER_OF_UNIQUE_USERS,
+                "0");
 
         assertApplianceMetricsDetailsMatch(expectedMetrics, "appliance0");
 
         archivePV();
 
         assertApplianceMetricsDetailsMatch(expectedMetrics, "appliance0");
+    }
+
+    @Test
+    void testPVDetails() {
+
+        Map<String, String> expectedMetrics = Map.ofEntries(
+                entry(mgmtSource + "PV Name", pvName),
+                entry(mgmtSource + "Instance archiving PV", "appliance0"),
+                entry(mgmtSource + "Archiver DBR type (from typeinfo):", "DBR_SCALAR_DOUBLE"),
+                entry(mgmtSource + "Is this a scalar:", "Yes"),
+                entry(mgmtSource + "Number of elements:", "1"),
+                entry(mgmtSource + "Precision:", "0.0"),
+                entry(mgmtSource + "Units:", "null"),
+                entry(mgmtSource + "Is this PV paused:", "No"),
+                entry(mgmtSource + "Sampling method:", "MONITOR"),
+                entry(mgmtSource + "Sampling period:", "1.0"),
+                entry(mgmtSource + "Are we using PVAccess?", "No"),
+                entry(pvSource + "Host name", "localhost"),
+                entry(pvSource + "Controlling PV", ""),
+                entry(pvSource + "Is engine currently archiving this?", "yes"),
+                entry(pvSource + "Sample buffer capacity", "3"),
+                entry(etlSource + "Name (from ETL)", pvName),
+                entry(retrievalSource + RetrievalMetrics.NUMBER_OF_RETRIEVAL_REQUESTS, "0"),
+                entry(retrievalSource + RetrievalMetrics.NUMBER_OF_UNIQUE_USERS, "0"));
+
+        archivePV();
+
+        assertPVDetailsMatch(expectedMetrics);
     }
 }
